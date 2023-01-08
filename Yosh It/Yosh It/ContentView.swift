a50a7fd5f4ea816b321a25c8560c34a11aff41a4
Alex Yoshida <alexyoshida264@gmail.com>
2023-01-08 15:31:16 -0500

    inital project

---
+++ Yosh It/Yosh It/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Yosh It
+//
+//  Created by Alex Yoshida on 2023-01-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Yosh It/Yosh It/Yosh_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yosh_ItApp.swift
+//  Yosh It
+//
+//  Created by Alex Yoshida on 2023-01-08.
+//
+
+import SwiftUI
+
+@main
+struct Yosh_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
